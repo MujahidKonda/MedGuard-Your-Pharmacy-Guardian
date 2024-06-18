23e397ae147a39513d79d08ac603436303a7279a
MujahidKonda <kondamujahid.nbnscoe.comp@gmail.com>
2024-06-18 23:50:45 +0530

    18-06-2024

---
+++ app/src/main/java/com/android/pharmware/Utility.java
@@ -0,0 +1,29 @@
+package com.android.pharmware;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class Utility {
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = (ListAdapter) listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
+}
